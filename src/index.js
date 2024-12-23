2dc3a4f5e70131d6f3101388f8da790ee3a878b0
Ahsaan-ullah3236 <ahsan6377202@gmail.com>
2024-12-23 13:15:10 +0500

    connect to mongoose atlest

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// import('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+// one way connect db with mongoose and the second way is proffessional  create a different db file and import file in this file
+/*import express from "express";
+const app = express()
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERROR:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listing on Port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR:", error);
+    throw error;
+  }
+})();
+*/
